3f4b6cf561921e3f8a155002ad13bf2a97a75094
Adnan <adnanneyazi88@gmail.com>
2024-11-04 19:02:29 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  get 'home', to: 'home#index'
+  devise_for :users, path: '', path_names: {
+                                 sign_in: 'login',
+                                 sign_out: 'logout',
+                                 registration: 'signup'
+                               },
+                     controllers: {
+                       sessions: 'users/sessions',
+                       registrations: 'users/registrations'
+                     }
+end
